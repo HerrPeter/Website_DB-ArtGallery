b55058d0a6d2866c9ed84eab75a459a64d4157ad
Peter Kaufinger <peterkaufinger@gmail.com>
2019-05-10 20:47:23 -0700

    Added btn

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: "Nothing"
+    };
+  }
+
+  //   connectToDB = () => {
+  //     console.log("Connecting...");
+  //     const connection = SQL.createConnection({
+  //       host: "35.222.200.150",
+  //       user: "root",
+  //       password: "root"
+  //     });
+
+  //     connection.connect(err => {
+  //       if (err) {
+  //         console.log(`Error Connecting: ${err}`);
+  //         return;
+  //       } else {
+  //         // Success
+  //         console.log(`Connected as ID: ${connection.threadId}`);
+  //       }
+  //     });
+  //   };
+
   render() {
-    return <h1>Art Show Gallery (Database)</h1>;
+    return (
+      <div>
+        <h1>Art Show Gallery (Database)</h1>
+        <h3>{this.state.results}</h3>
+        <Button onClick={() => alert("click")}>Click Me</Button>
+      </div>
+    );
   }
 }
