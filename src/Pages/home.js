afc3d6526841cc165c3e6dbfe60a273a7ec681db
Peter Kaufinger <peterkaufinger@gmail.com>
2019-05-11 23:15:51 -0700

    Connection to Server Working (locally)

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -18,37 +18,10 @@ export default class Home extends React.Component {
     fetch("http://localhost:4000")
       .then(response => response.json())
       .then(({ data }) => {
+        this.setState({ results: data });
         console.log(data);
       })
       .catch(err => console.log(err));
-
-    // fetch("192.168.0.21/artists")
-    //   .then(results => {
-    //     results = results.json();
-    //     console.log(results);
-    //     return results;
-    //   })
-    //   .then(
-    //     data => {
-    //       let stuff = data;
-    //       console.log("Succ...");
-    //       console.log(stuff);
-    //     },
-    //     err => {
-    //       console.log("Fail...");
-    //       console.log(err);
-    //     }
-    //   );
-
-    // fetch("www.project-jimbo.appspot.com/artists")
-    //   .then(response => {
-    //       console.log(`1) Response: ${response}`);
-    //     response.json();
-    //   })
-    //   .then(data => {
-    //     console.log(data);
-    //   })
-    //   .catch(err => console.error(err));
   };
 
   render() {
