b0e0b8d702553c5f30ddb65a2911ab5e58c4a55d
Peter Kaufinger <peterkaufinger@gmail.com>
2019-05-22 00:59:58 -0700

    Started creating grid/table to distplay DB results

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import BootstrapTable from "reactjs-bootstrap-table";
+require("bootstrap/dist/css/bootstrap.css");
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      results: "Nothing"
+      results: [],
+      columns: []
     };
   }
 
@@ -15,21 +18,41 @@ export default class Home extends React.Component {
   }
 
   getArtists = () => {
-    fetch("http://localhost:4000")
+    fetch("http://192.168.0.21:4000/artists?name=halIE+tuft")
       .then(response => response.json())
-      .then(({ data }) => {
-        this.setState({ results: data });
+      .then(({ data, columns }) => {
+        this.setState({ results: data, columns: columns });
         console.log(data);
       })
       .catch(err => console.log(err));
   };
 
+  // Custom draw row for one artist.
+  renderArtist = ({ artist_id, name }) => {
+    return <p key={artist_id}>{`Id: ${artist_id} Name: ${name}`}</p>;
+  };
+
+  // Custom draw first row of a table.
+  renderRowOne = (col, index) => {
+    return <col key={index}>{col.name}</col>;
+  };
+
   render() {
     return (
       <div>
-        <h1>Art Show Gallery (Database)</h1>
-        <h3>{this.state.results}</h3>
-        <Button onClick={() => alert("click")}>Click Me</Button>
+        <h1>Art Show Gallery Database</h1>
+        <BootstrapTable
+          headers={true}
+          columns={[{ name: "artist_id" }, { name: "name" }]}
+          data={this.state.results}
+        />
+
+        {/* Works but is replaced by BootstrapTable. */}
+        {/* <div>
+          {this.state.results.map(currItem => this.renderArtist(currItem))}
+        </div> */}
+
+        <Button onClick={() => this.getArtists()}>Click Me</Button>
       </div>
     );
   }
