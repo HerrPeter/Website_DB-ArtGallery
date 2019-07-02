f56d6115f8a420642c6a2dfbf2cbca0fe9f6754f
HerrPeter <peterkaufinger@gmail.com>
2019-07-02 03:14:56 -0700

    Added column drop down & input for search value components

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -5,11 +5,12 @@ import { Table } from "reactstrap";
 import Select from "react-select";
 
 // -- My stuff -- //
-import ART_SERVER from "../misc/server-info";
+import { ART_SERVER } from "../misc/server-info";
 import { RenderRowOne, RenderRecords } from "../components/table";
 
 // -- CSS Styles -- //
 import "../styles/pages/home.css";
+import '../styles/components/inputStyles.css';
 
 // -- Home Page Class -- //
 export default class Home extends React.Component {
@@ -20,28 +21,39 @@ export default class Home extends React.Component {
       records: [],
       columns: [],
       db_options: {
-        table: null
-      }
+        table: null,
+        column: null,
+        searchVal: '',
+      },
+      columnOptions: ART_SERVER.database.tables[0].columns,
     };
   }
 
   // FETCH Req: Get data from DB given user selected options (i.e. Table).
-  getArtGalleryData = () => {
-    const { table } = this.state.db_options;
+  runSearch = () => {
+    const { table, column, searchVal } = this.state.db_options;
     if (!table) {
+      console.log('No db table selected.')
+      return;
+    } else if (!column) {
+      console.log('No table column selected.');
+      return;
+    } else if (!searchVal) {
+      console.log('No search value.')
       return;
     }
 
     // Encode query (INCOMPLETE).
     //const query = btoa(`table=${table.value}`);
-    const query = `table=${table.value}`;
+    const query = `table=${table.value}&searchCol=${column.value}&searchVal=${searchVal}`;
 
-    fetch(`${ART_SERVER.host}/artists?${query}`)
+    fetch(`${ART_SERVER.host}/ag_db/search?${query}`)
       .then(response => response.json())
       .then(({ data, columns }) => {
         this.setState({ records: data, columns: columns });
-        console.log(`FETCHED Data:`);
-        console.log(data);
+        // console.log(`FETCHED Data:`);
+        // console.log(data);
+        // console.log(columns);
       })
       .catch(err => {
         console.log(err);
@@ -49,31 +61,102 @@ export default class Home extends React.Component {
       });
   };
 
-  // Handle what happens when user selects option from dropdown menu (db tables).
-  handleTableDropdownChange = selected => {
-    this.setState({
-      db_options: {
-        table: selected
+  // Handle table dropdown component changes.
+  handleTableDropdownChange = async selected => {
+    let colOptions = ART_SERVER.database.tables[0].columns;
+
+    // Fetch/Cache column data.
+    if (ART_SERVER.database.tables[selected.index].columns.length === 0) {
+      // Fetch the column data.
+      let columns = await this.getColumnsArrayAsync(selected.value);
+      if (columns) {
+        colOptions = columns;
+        ART_SERVER.database.tables[selected.index].columns = columns;
       }
+    } else {
+      // Just change to the active column data.
+      colOptions = ART_SERVER.database.tables[selected.index].columns;
+    }
+    // Update the state of the home page.
+    let db_options = this.state.db_options;
+    db_options.table = selected;
+    db_options.column = null;
+    this.setState({
+      db_options: db_options,
+      columnOptions: colOptions,
     });
   };
 
+  // Get columns from DB for selected table.
+  getColumnsArrayAsync = async selectedTable => {
+    let columns = null;
+    try {
+      let response = await fetch(`${ART_SERVER.host}/ag_db?table=${selectedTable}`);
+      let { data } = await response.json();
+      columns = data.map((columnData) => {
+        if (columnData.Field) {
+          return { value: columnData.Field, label: columnData.Field };
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+
+    return columns;
+  }
+
+  // Handle column dropdown component changes.
+  handleColDropdownChange = selected => {
+    // NOTE: This preserves the adjacent db_options params within the state var.
+    let db_options = this.state.db_options;
+    db_options.column = selected;
+
+    this.setState({
+      db_options: db_options
+    })
+  }
+
+  // Handle text input changes for search value.
+  handleSearchInputChange = stuff => {
+    let db_options = this.state.db_options;
+    db_options.searchVal = stuff.target.value;
+    db_options.searchVal = db_options.searchVal.replace(' ', '+');
+
+    this.setState({
+      db_options: db_options
+    })
+  }
+
   render() {
-    const { records, columns, db_options } = this.state;
+    const { records, columns, db_options, columnOptions } = this.state;
 
     return (
       <div>
         <h1>Art Show Gallery Database</h1>
-        <div class="row">
-          <div class="column">
+        <div className="row">
+          <div className="column">
             <Select
               value={db_options.table}
               options={ART_SERVER.database.tables}
               onChange={this.handleTableDropdownChange}
+              placeholder='Select DB Table'
             />
           </div>
-          <div class="column">
-            <Button onClick={this.getArtGalleryData}>Run Search</Button>
+          <div className="column">
+            <Select
+              value={db_options.column}
+              options={columnOptions}
+              onChange={this.handleColDropdownChange}
+              placeholder='Select Table Column'
+            />
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='column'>
+            <input className='textInput' onChange={this.handleSearchInputChange} value={this.state.text} placeholder='Search Value' />
+            <Button id='buttonInput' onClick={this.runSearch}>Run Search</Button>
           </div>
         </div>
 
